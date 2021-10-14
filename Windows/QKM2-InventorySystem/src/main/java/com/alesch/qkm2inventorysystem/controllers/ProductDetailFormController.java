e6a8b356ab489ece02c1df3ff484da31d514135d
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2021-10-13 23:01:02 -0700

    Created ProductDetailFormController & designer file.

---
+++ Windows/QKM2-InventorySystem/src/main/java/com/alesch/qkm2inventorysystem/controllers/ProductDetailFormController.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Anthony Chavez.
+ */
+
+package com.alesch.qkm2inventorysystem.controllers;
+
+public class ProductDetailFormController {
+
+    //<editor-fold desc="Fields">
+
+    private final int productId;
+
+    //</editor-fold>
+
+    public ProductDetailFormController() {
+        productId = -1;
+    }
+
+    public ProductDetailFormController(int productId) {
+        this.productId = productId;
+    }
+
+}
